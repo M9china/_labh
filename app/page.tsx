b8ddcbb34e825e0a1d5b8c1174f27e71b2b116c8
M9china <nqobilencaza2@gmail.com>
2024-08-30 00:58:21 +0200

    fix: css enhancements

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import { Hero } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between">
       <Hero/>
-    </main>
   );
 }
