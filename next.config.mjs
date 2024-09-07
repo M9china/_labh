995320316ff57c37270fddd400cbaca4270f71dc
M9china <nqobilencaza2@gmail.com>
2024-09-07 23:26:36 +0200

    feat: implementing auth

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {output: "standalone",  experimental: {
+    externalMiddlewareRewritesResolve: true,
+  },};
 
 export default nextConfig;
---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { auth } from "./lib"
+
+
+export default auth((req:any) => {
+ if (!req.auth && protectedRoutes.includes(req.nextUrl.pathname)) {
+    const url = req.url.replace(req.nextUrl.pathname, "/api/auth/signin")
+    return Response.redirect(url)
+ }
+})
+
+export const config = {
+ matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
+
+export const protectedRoutes = [
+ '/collection',
+ '/cart',
+]
