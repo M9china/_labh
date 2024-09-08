1e2a23c91b798d20eae18b1d946c2854ac299ed3
M9china <nqobilencaza2@gmail.com>
2024-09-08 20:14:38 +0200

    fix: build errors

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined |any;
+declare global { var prisma: PrismaClient | undefined | any;
 }
 
 export const db = globalThis.prisma || new PrismaClient();
