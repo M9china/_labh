f5461c24ff92b0973bd02878db3c16dc9333b034
M9china <nqobilencaza2@gmail.com>
2024-09-12 07:48:41 +0200

    refactor: Update FilterStaticData and Filters components

--- src/components/Collection/FilterStaticData.ts
+++ src/components/Collection/FilterStaticData.ts
@@ -20,26 +20,26 @@ export const filters = [
 
 export const allItems = {
   all: [
-    { id: 1, name: "Basic Tee", price: "450", image: "/labh (6).jpeg" },
-    { id: 2, name: "Long sleeves", price: "350", image: "/labh (20).jpeg" },
-    { id: 3, name: "Graphic Tee", price: "250", image: "/labh (13).jpeg" },
-    { id: 4, name: "Confy Sweater", price: "400", image: "/" },
-    { id: 5, name: "Tote Bag", price: "300", image: "/" },
-    { id: 6, name: "Bucket Hat", price: "250", image: "/labh (9).jpeg" },
-    { id: 7, name: "Baseball Cap", price: "250", image: "/" },
-    { id: 8, name: "Jogger Pants", price: "500", image: "/" },
-    { id: 9, name: "Cargo Pants", price: "560", image: "/" },
-    { id: 10, name: "Classic Hoodie", price: "300", image: "/" },
-    { id: 11, name: "Oversized Hoodie", price: "300", image: "/" },
-    { id: 12, name: "Tights", price: "150", image: "/labh (10).jpeg" },
-    { id: 13, name: "Bennies", price: "150", image: "/labh (4).jpeg" },
+    { id: 1, name: "Basic Tee", price: "", image: "/labh (6).jpeg" },
+    { id: 2, name: "Long sleeves", price: "", image: "/labh (20).jpeg" },
+    { id: 3, name: "Graphic Tee", price: "", image: "/labh (13).jpeg" },
+    { id: 4, name: "Confy Sweater", price: "", image: "/" },
+    { id: 5, name: "Tote Bag", price: "", image: "/" },
+    { id: 6, name: "Bucket Hat", price: "", image: "/labh (9).jpeg" },
+    { id: 7, name: "Baseball Cap", price: "", image: "/" },
+    { id: 8, name: "Jogger Pants", price: "", image: "/" },
+    { id: 9, name: "Cargo Pants", price: "", image: "/" },
+    { id: 10, name: "Classic Hoodie", price: "", image: "/" },
+    { id: 11, name: "Oversized Hoodie", price: "", image: "/" },
+    { id: 12, name: "Tights", price: "", image: "/labh (10).jpeg" },
+    { id: 13, name: "Bennies", price: "", image: "/labh (4).jpeg" },
   ],
   tees: [
     { id: 1, name: "Basic Tee", price: "", image: "/" },
     { id: 2, name: "Graphic Tee", price: "", image: "/" },
   ],
   classic: [
-    { id: 13, name: "", price: "", image: "/" }
+    { id: 13, name: "Classic Hoodie", price: "", image: "/" }
 ],
   sweaters: [
     { id: 4, name: "Comfy Sweater", price: "", image: "/" },
@@ -49,7 +49,7 @@ export const allItems = {
     { id: 5, name: "Tote Bag", price: "", image: "/" }
 ],
   exclusive: [
-    { id: 6, name: "Exclusive Hat", price: "", image: "/" }
+    { id: 6, name: "Exclusive ", price: "", image: "/" }
 ],
   headwear: [
     { id: 7, name: "Baseball Cap", price: "", image: "/" }
@@ -59,7 +59,7 @@ export const allItems = {
     { id: 9, name: "Cargo Pants", price: "", image: "/" },
   ],
   hoodies: [
-    { id: 10, name: "Classic Hoodie", price: "", image: "/" },
-    { id: 11, name: "Oversized Hoodie", price: "", image: "/" },
+    { id: 10, name: "Relaxed Fit", price: "", image: "/" },
+    { id: 11, name: "Oversized", price: "", image: "/" },
   ],
 };
--- src/components/Collection/Filters.tsx
+++ src/components/Collection/Filters.tsx
@@ -50,7 +50,7 @@ export const Filters = () => {
                 {filteredItems.length > 0 ? (
                     filteredItems.map((item) => (
                         <Link href={'/'} key={item.id} className=''>
-                            <Image className='h-[15rem] w-full object-cover rounded mt-4' alt={`${item.name} image`} src={item.image} width={1500} height={1500}/>
+                            <Image className='h-[15rem] border w-full object-cover rounded mt-4' alt={`${item.name} image`} src={item.image} width={1500} height={1500}/>
                             <h2 className='text-md text-black font-medium mt-2 text-lg overflow-ellipsis'>{item.name}</h2>
                             <p className='text-md text-gray-800 font-medium text-md'>R {item.price}</p>
                         </Link>
