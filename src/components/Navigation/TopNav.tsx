7a48fe1f425f2542b25235bd483394888f98fccc
M9china <nqobilencaza2@gmail.com>
2024-09-19 18:07:37 +0200

    feature: implement logout functionality

--- src/components/Navigation/TopNav.tsx
+++ src/components/Navigation/TopNav.tsx
@@ -15,9 +15,13 @@ import {
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Fragment, useState } from "react";
 import { navigation } from "./NavStatic";
-
-export const TopNav = () => {
+import { signOut } from "next-auth/react";
+export const TopNav = ({user}: any) => {
   const [open, setOpen] = useState(false);
+
+  const handleAuth = () => {
+    return signOut({ callbackUrl: "/" });
+  };
 
   return (
     <header className="sm:py-2 bg-white fixed border-b z-50 top-0 sm:w-full w-screen">
@@ -46,16 +50,17 @@ export const TopNav = () => {
 
             {/* Links */}
             <TabGroup className="">
-
               <TabPanels as={Fragment}>
                 {navigation.categories.map((category) => (
                   <TabPanel
                     key={category.name}
                     className="space-y-10 px-4 pb-8 pt-10"
                   >
-                  <Link href={category.href}>
-                  <span className='text-black hover:underline'>{category.name}</span>
-                  </Link>
+                    <Link href={category.href}>
+                      <span className="text-black hover:underline">
+                        {category.name}
+                      </span>
+                    </Link>
                   </TabPanel>
                 ))}
               </TabPanels>
@@ -63,19 +68,29 @@ export const TopNav = () => {
 
             <div className="space-y-6 border-t border-gray-200 px-4 py-6">
               <div className="flow-root">
-                <Link
-                  href="/api/auth/signin"
-                  className="-m-2 block p-2 font-medium text-gray-900"
-                >
-                  Sign in
-                </Link>
+                {user ? (
+                  <Link
+                    href="/"
+                    onClick={handleAuth}
+                    className="-m-2 block p-2 font-medium text-gray-900"
+                  >
+                    Sign Out
+                  </Link>
+                ) : (
+                  <Link
+                    href="/api/auth/signin"
+                    className="-m-2 block p-2 font-medium text-gray-900"
+                  >
+                    Sign in
+                  </Link>
+                )}
               </div>
               <div className="flow-root">
                 <Link
-                  href="#"
+                  href="/profile"
                   className="-m-2 block p-2 font-medium text-gray-900"
                 >
-                  Create account
+                  Profile
                 </Link>
               </div>
             </div>
@@ -102,18 +117,28 @@ export const TopNav = () => {
 
             <div className="ml-auto flex items-center">
               <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                <Link
-                  href="/api/auth/signin"
-                  className="text-md font-medium text-gray-700 hover:text-gray-800"
-                >
-                  Sign in
-                </Link>
+              {user ? (
+                  <Link
+                    href="/"
+                    onClick={handleAuth}
+                    className="-m-2 block p-2 font-medium text-gray-900"
+                  >
+                    Sign Out
+                  </Link>
+                ) : (
+                  <Link
+                    href="/api/auth/signin"
+                    className="-m-2 block p-2 font-medium text-gray-900"
+                  >
+                    Sign in
+                  </Link>
+                )}
                 <span aria-hidden="true" className="h-6 w-px bg-gray-200" />
                 <Link
-                  href="#"
+                  href="/profile"
                   className="text-md font-medium text-gray-700 hover:text-gray-800"
                 >
-                  Create account
+                  Profile
                 </Link>
               </div>
 
@@ -166,4 +191,4 @@ export const TopNav = () => {
       </nav>
     </header>
   );
-};
+}
