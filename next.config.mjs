d823f818a3cc4f2fb9245c54dcac57dc18b93a46
M9china <nqobilencaza2@gmail.com>
2024-09-20 11:58:07 +0200

    refactor: Update next.config.mjs to include remotePatterns for images

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {output: "standalone",
     experimental: {
         externalMiddlewareRewritesResolve: true,
       },
+      images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: "lh3.googleusercontent.com",
+            port: '',
+          },
+        ],
+      }
 };
 
 export default nextConfig;
