28ce75b96e5aae782b9332f43d7a2e2891287e10
M9china <nqobilencaza2@gmail.com>
2024-10-07 11:54:11 +0200

    feat: add to cart function

--- src/components/Collection/SelectedItem.tsx
+++ src/components/Collection/SelectedItem.tsx
@@ -1,22 +1,43 @@
-"use client";
+'use client';
+
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { useParams, useRouter } from "next/navigation";
 import { allItems } from "./FilterStaticData";
 import Image from "next/image";
-import { Item } from "./FilterDto";
+import { useSession, signIn } from "next-auth/react";
+import { IItem } from "./FilterDto";
+import { addToCart } from "@/actions";
 
-export const SelectedItem = () => {
+export const SelectedItem: React.FC<IItem> = ({ productId }) => {
   const router = useRouter();
   const { id } = useParams();
+  const { data: session } = useSession();
 
   // Find the item based on the ID from the URL
   const item = Object.values(allItems)
     .flat()
-    .find((i) => i.id === id) as Item;
+    .find((i) => i.productId === id);
 
   if (!item) {
     return <p>Item not found.</p>;
   }
+
+  const handleAddToCart = async () => {
+    if (!session) {
+      signIn(); 
+      return;
+    }
+
+    try {
+      // Call the server action to add the item to the cart
+      await addToCart(item.productId, 1);
+      console.log('item successfully added',item.productId) 
+      alert('Item added to cart successfully!');
+    } catch (error) {
+      console.error('Error adding item to cart:', error);
+      alert('There was an error adding the item to the cart.');
+    }
+  };
 
   return (
     <div className="pt-[5rem] bg-white px-4">
@@ -30,7 +51,7 @@ export const SelectedItem = () => {
       <div className="mt-5">
         <Image
           className="h-[23rem] rounded object-cover"
-          src={item.image}
+          src={item.image as string}
           width={1500}
           height={1500}
           alt={`${item.name} image`}
@@ -45,36 +66,39 @@ export const SelectedItem = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1"
+              strokeWidth="1"
               stroke="currentColor"
               className="size-6"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
               />
             </svg>
           </div>
         </div>
-        <div className="bg-blue-600 text-white flex space-x-1 justify-center items-center rounded mt-5">
-          <button className="font-semibold rounded px-4 py-2">
-            Add to cart
-          </button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="size-6"
+        <div className="bg-blue-600 text-white rounded flex justify-center mt-5">
+          <button
+            onClick={handleAddToCart}
+            className="font-semibold rounded px-4 py-2 flex space-x-1 items-center"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-            />
-          </svg>
+            <span>Add to cart</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </div>
